6255078de00642375bc5ad3d31187f48505b50b1
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-09-19 07:05:18 -0400

    Update kofiframe.php

--- widgets/views/kofiframe.php
+++ widgets/views/kofiframe.php
@@ -16,7 +16,7 @@ use humhub\widgets\PanelMenu;
   <div class="panel-body">
 
 <?= Html::beginTag('div') ?>
-<script type='text/javascript' src='https://ko-fi.com/widgets/widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#46b798', '<?= $ko_fiUrl ?>');kofiwidget2.draw();</script> 
+<script type='text/javascript' src='https://ko-fi.com/widgets/widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '<?= $color ?>', '<?= $ko_fiUrl ?>');kofiwidget2.draw();</script> 
 <?= Html::endTag('div'); ?>
 </div>
 </div>
