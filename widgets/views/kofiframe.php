6d51aacf877a10d502f0839ec5005264970e71ad
Ace2213 <Yamman92@gmail.com>
2019-11-01 20:23:01 -0300

    Update kofiframe.php
    1. Some styling might be unnecessary
    2. Might require additional changes in the module
    3. Background-color and user-id are left blank because they are user defined in admin config and I don't know how to implement that

--- widgets/views/kofiframe.php
+++ widgets/views/kofiframe.php
@@ -16,7 +16,13 @@ use humhub\widgets\PanelMenu;
   <div class="panel-body">
 
 <?= Html::beginTag('div') ?>
-<script type='text/javascript' src='https://ko-fi.com/widgets/widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '<?= $color ?>', '<?= $ko_fiUrl ?>');kofiwidget2.draw();</script> 
+<div class="btn-container">
+<a title="Support me on ko-fi.com" style="background-color:[user-defined-value];box-shadow: 1px 1px 0px rgba(0, 0, 0, 0.2);line-height: 36px!important;min-width: 150px;display: inline-block!important;padding: 2px 12px !important;text-align: center !important;border-radius: 7px;color: #fff;cursor: pointer;overflow-wrap: break-word;vertical-align: middle;border: 0 none #fff !important;" href="https://ko-fi.com/[user-id]" target="_blank">
+<span style="color: #fff !important;letter-spacing: -0.15px!important;text-wrap: none;vertical-align: middle;line-height: 33px !important;padding: 0;text-align: center;text-decoration: none!important;text-shadow: 0 1px 1px rgba(34, 34, 34, 0.05)">
+<img src="https://ko-fi.com/img/cup-border.png" class="kofiimg" style="height:20px;vertical-align:-4px">Buy Us a Ko-fi
+</span>
+</a>
+</div>
 <?= Html::endTag('div'); ?>
 </div>
 </div>
