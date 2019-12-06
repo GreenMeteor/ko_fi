856375bdce3be214dda482514269a4ec9cda7204
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-12-05 23:06:39 -0500

    Update base.php

--- messages/en-GB/base.php
+++ messages/en-GB/base.php
@@ -1,6 +1,8 @@
 <?php
     return [
-        '<strong>Ko-fi</strong>' => '<strong>Ko-fi</strong> ',
+        'Text' => 'Text',
+        'e.g: https://ko-fi.com/<strong>{id}</strong>' => 'e.g: https://ko-fi.com/<strong>{id}</strong>',
+        'e.g: https://az743702.vo.msecnd.net/cdn/kofi{number}.png?v=2' => 'e.g: https://az743702.vo.msecnd.net/cdn/kofi{number}.png?v=2',
         'Ko-fi Settings' => 'Ko-fi Settings',
         'Ko-fi URL:' => 'Ko-fi URL:',
         '<strong>Ko-fi</strong> module configuration' => '<strong>Ko-fi</strong> module configuration',
