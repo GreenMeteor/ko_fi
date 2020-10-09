090be1e2c02899fa8c072aad82fc855ebe21eb8b
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-10-09 12:59:08 -0400

    Fix: Title text field

--- views/admin/index.php
+++ views/admin/index.php
@@ -2,23 +2,24 @@
 
 use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
+
 ?>
 
 <div class="panel panel-default">
 
-    <div class="panel-heading"><?= '<strong>Ko-fi</strong> module configuration' ?></div>
+    <div class="panel-heading"><?= Yii::t('KofiModule.base', '<strong>Ko-fi</strong> module configuration'); ?></div>
 
     <div class="panel-body">
 
         <?php $form = ActiveForm::begin(['id' => 'configure-form']); ?>
         <div class="form-group">
-            <?= $form->field($model, 'text'); ?>
+            <?= $form->field($model, 'text')->textInput(['class' => 'form-control', 'placeholder' => 'Title', 'disabled' => false])->label(false) ?>
             <?= $form->field($model, 'serverUrl'); ?>
             <?= $form->field($model, 'color'); ?>
         </div>
 
         <div class="form-group">
-            <?= Html::submitButton('Save', ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
+            <?= Html::submitButton(Yii::t('KofiModule.base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
         </div>
 
         <?php ActiveForm::end(); ?>
