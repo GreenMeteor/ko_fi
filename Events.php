cc172f326817ccca3aeab32aed60c5c9bd03de01
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-12-11 19:29:28 -0500

    Enh: Add AdminMenu permission restrictions
    See https://github.com/humhub/humhub/issues/4695#issuecomment-742251519

--- Events.php
+++ Events.php
@@ -5,28 +5,33 @@ namespace humhub\modules\ko_fi;
 use Yii;
 use yii\helpers\Url;
 use yii\base\BaseObject;
-use humhub\models\Setting;
+use humhub\modules\ui\menu\MenuLink;
+use humhub\modules\admin\widgets\AdminMenu;
+use humhub\modules\admin\permissions\ManageModules;
 
 class Events extends BaseObject
 {
 
     public static function onAdminMenuInit($event)
     {
-        $event->sender->addItem([
-            'label' => 'Ko-fi Settings',
+        if (!Yii::$app->user->can(ManageModules::class)) {
+            return;
+        }
+
+        /** @var AdminMenu $menu */
+        $menu = $event->sender;
+
+        $menu->addEntry(new MenuLink([
+            'label' => Yii::t('base', 'Ko-fi Settings'),
             'url' => Url::toRoute('/ko_fi/admin/index'),
-            'group' => 'settings',
             'icon' => '<i class="fa fa-coffee"></i>',
             'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'ko_fi' && Yii::$app->controller->id == 'admin',
             'sortOrder' => 650
-        ]);
+        ]));
     }
 
     public static function addKofiFrame($event)
     {
-        $event->sender->view->registerAssetBundle(assets\Assets::class);
-        $event->sender->addWidget(widgets\KofiFrame::class, [], [
-            'sortOrder' => Setting::Get('timeout', 'ko_fi')
-        ]);
+        $event->sender->addWidget(widgets\KofiFrame::class, [], ['sortOrder' => Yii::$app->getModule('ko_fi')->settings->get('sortOrder')]);
     }
 }
