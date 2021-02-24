3304e876d0a4e6acfae601040e8a6ca0b4e5d719
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2021-02-24 07:06:57 -0500

    Enh: New Ko-fi widget

--- widgets/views/kofiframe.php
+++ widgets/views/kofiframe.php
@@ -1,23 +1,23 @@
 <?php
 
-use yii\helpers\Url;
 use humhub\libs\Html;
-use humhub\models\Setting;
-use humhub\widgets\PanelMenu;
-use humhub\modules\ko_fi\assets\Assets;
 
-Assets::register($this);
+$urlJs = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+$this->registerJsFile($urlJs);
 ?>
 
 <div class="panel panel-default ko_fi-snippet" id="ko_fi-snippet">
-    <?= PanelMenu::widget(['id' => 'ko_fi-snippet']); ?>
-  <div class="panel-heading">
-    <p><?= $text; ?></p>
-  </div>
-  <div class="panel-body">
+    <?= Html::beginTag('div') ?>
+    <!--<a href='<?= $ko_fiUrl ?>' target='_blank'><img height='36' style='border:0px;height:36px;' src='<?= $color ?>' border='0' alt='Buy us a coffee.' /></a>-->
+    <script src='<?= $urlJs ?>'></script>
+    <script>
+    kofiWidgetOverlay.draw('<?= $ko_fiUrl; ?>', {
+        'type': 'floating-chat',
+        'floating-chat.donateButton.text': '<?= $text; ?>',
+        'floating-chat.donateButton.background-color': '<?= $color ?>',
+        'floating-chat.donateButton.text-color': '#fff'
 
-<?= Html::beginTag('div') ?>
-<a href='<?= $ko_fiUrl; ?>' target='_blank'><img height='36' style='border:0px;height:36px;' src='<?= $color; ?>' border='0' alt='Buy us a coffee.' /></a>
-<?= Html::endTag('div'); ?>
-</div>
+    });
+    </script>
+    <?= Html::endTag('div'); ?>
 </div>
